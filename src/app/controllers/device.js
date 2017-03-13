7fe652612e492f8be3cd0934a4b979b959ea1de9
asouza <ardsj@yahoo.com.br>
2017-03-13 15:57:38 -0300

    usando lambda

--- src/app/controllers/device.js
+++ src/app/controllers/device.js
@@ -1,19 +1,18 @@
 var express = require('express'),
-  router = express.Router(),
-  Promise = require('bluebird')
-  adb = require('adbkit')
-  var adbClient = adb.createClient()
-  adbCli = require('adb-cli')
-  apk = 'vendor/app.apk';
+router = express.Router(),
+Promise = require('bluebird')
+adb = require('adbkit')
+adbClient = adb.createClient()
+adbCli = require('adb-cli');
 
-module.exports = function (app) {
+module.exports = (app) => {
   app.use('/', router);
 };
 
-router.get('/devices', function (req, res, next) {
+router.get('/devices', (req, res, next) => {
 
   adbClient.listDevices()
-    .then(function(devices) {
+    .then((devices) => {
         console.log(devices)
         res.json(devices)
     })
